318da960455e2977ed92bcbb512698e5b64a73f5
iram96 <mariano.maselli@virgilio.it>
2022-03-15 18:23:19 +0100

    Added home with menu

---
+++ resources/views/home.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <ul>
+            {{-- @forelse ($menu as $item)
+                <li>
+                    <a href="{{ route(`${item}`) }}">{{$item}}</a>
+                </li>
+            @empty
+                <h3>No menu</h3>
+            @endforelse --}}
+            <li>
+                <a href="{{route('comics')}}"> Comics</a>
+            </li>
+        </ul>
+    </header>
+
+    <h1>ciao</h1>
+    
+</body>
+</html>
